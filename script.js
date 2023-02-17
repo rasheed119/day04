6adb8a5d68a61c3cee8cd6cee6dda417cedbdca3
rasheed119 <78138853+rasheed119@users.noreply.github.com>
2023-02-17 20:01:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ request.onload=function(){
     var result = JSON.parse(request.response);
     console.log(result);
     for(var i = 0; i < result.length ; i++){
-        console.log(result[i].flag)
+        console.log(result[i].name,result[i].region,result[i].subregion,result[i].population)
     }
 }
